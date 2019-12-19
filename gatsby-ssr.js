6a203ba009044a36193832f15f980618d5c9e5db
Aissa BOUGUERN <aissa.bouguern@gmail.com>
2019-12-19 15:18:45 -0500

    Add OneSignal SDK

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,20 +1,22 @@
-import React from 'react';
+const React = require('react');
 
 exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
   if (process.env.NODE_ENV === `production`) {
     return setHeadComponents([
       <script
-        key={`gatsby-onesignal`}
+        src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
+        async=""
+      ></script>,
+      <script
         dangerouslySetInnerHTML={{
           __html: `
-            <script type="text/javascript">
-            var OneSignal = window.OneSignal || [];
-            OneSignal.push(function() {
-                OneSignal.init({
-                  appId: "e99d79a5-fc67-4a4a-8203-4fd24afd6d73",
-                });
-            </script>
-            <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+          var OneSignal = window.OneSignal || [];
+          OneSignal.push(function() {
+              OneSignal.init({
+                appId: "e99d79a5-fc67-4a4a-8203-4fd24afd6d73",
+              });
+          });
+            
             `
         }}
       />
