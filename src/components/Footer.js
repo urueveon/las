42aceb080a6f4d4b952c0caf5452ff29259e3523
Aissa BOUGUERN <aissa.bouguern@gmail.com>
2020-02-05 18:09:18 -0500

    Tweak footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,39 +7,42 @@ import gatsby from '../../content/thumbnails/gatsby.webp';
 export default class Footer extends Component {
   render() {
     return (
-      <footer className="footer container">
-        <div>
-          {/* <a href="https://ko-fi.com/taniarascia" target="_blank" rel="noopener noreferrer">
+      <footer className="footer">
+        <div className="container">
+          <div>
+            {/* <a href="https://ko-fi.com/taniarascia" target="_blank" rel="noopener noreferrer">
             Ko-Fi
           </a>
           <a href="https://patreon.com/taniarascia" target="_blank" rel="noopener noreferrer">
             Patreon
           </a> */}
-          <Link to="/newsletter">النشرة البريدية</Link>
-          <Link to="/privacy-policy">سياسة الخصوصية</Link>
-          <a href="/rss.xml" target="_blank" rel="noopener noreferrer">
-            RSS
-          </a>
-        </div>
-        <div>
-          <a href="https://www.netlify.com/" title="مستضاف من طرف Netlify">
-            <img
-              src={netlify}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="footer-img"
-              alt="GitHub"
-            />
-          </a>
-          <a href="https://www.gatsbyjs.org/" title="مبني بواسطة Gatsby">
-            <img
-              src={gatsby}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="footer-img"
-              alt="GitHub"
-            />
-          </a>
+            <Link to="/newsletter">النشرة البريدية</Link>
+            <Link to="/privacy-policy">سياسة الخصوصية</Link>
+            <a href="/rss.xml" target="_blank" rel="noopener noreferrer">
+              RSS
+            </a>
+          </div>
+
+          <div>
+            <a href="https://www.netlify.com/" title="مستضاف من طرف Netlify">
+              <img
+                src={netlify}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footer-img"
+                alt="GitHub"
+              />
+            </a>
+            <a href="https://www.gatsbyjs.org/" title="مبني بواسطة Gatsby">
+              <img
+                src={gatsby}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footer-img"
+                alt="GitHub"
+              />
+            </a>
+          </div>
         </div>
       </footer>
     );
