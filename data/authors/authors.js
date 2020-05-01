2d9a3010872ab0058c24030c52b7ab46273e64cc
urueveon <64614165+urueveon@users.noreply.github.com>
2020-05-01 22:01:10 +0200

    Update authors.js

--- data/authors/authors.js
+++ data/authors/authors.js
@@ -1,16 +1,14 @@
 import photo_aissa from './avatars/aissa.png';
 
 const aissa = {
-  name: 'عيسى محمد علي',
+  name: 'نتائج السودان',
   photo: photo_aissa,
   bio:
-    'مغربي من أصول أمازيغية، مطور ويب مستقل متخصص في الواجهات الأمامية، أحب التدوين وإغناء المحتوى التقني للغة الضاد وهذا كان السبب الرئيسي في إنشائي لمدونة توتومينا.',
+    'موقع مختص في تقديم وعرض نتائج الشهادة السودانية وشهادة الاساس ويساعد في استخراج الشهادة بصورة مجانية من وزارة التربية والتعليم.',
   website: '',
   social: {
-    facebook: '',
-    twitter: '',
-    github: 'https://github.com/aissa-bouguern',
-    linkedin: 'https://linkedin.com/in/bouguern'
+    facebook: 'https://web.facebook.com/sudansscr/',
+    telegram: 'https://t.me/sudansscr',
   }
 };
 export default { aissa };
